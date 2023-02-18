b11bb94b6ec06e0107fa5cdf84e421ccf3114589
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2023-02-17 22:23:00 -0800

    Correction made

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
+
 /**
- * main - program that prints numbers from 0 to 99.
+ * main - main block
+ * Description: prints all combinations of two two-digit numbers
  * Return: 0
  */
 
 int main(void)
 {
-	int c = 0;
+	int i, j;
+	int a, b, c, d;
 
-	while (c <= 99)
+	for (i = 0; i < 100; i++)
 	{
-		putchar(c / 10 + '0');
-		putchar(c % 10 + '0');
-		if (c != 99)
+		a = i / 10;
+		b = i % 10;
+
+		for (j = 0; j < 100; j++)
 		{
-			putchar(',');
-			putchar(' ');
+			c = j / 10;
+			d = j % 10;
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
 		}
-		c++;
 	}
-putchar('\n');
-return (0);
+	putchar(10);
+
+	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,20 @@
+#include <stdio.h>
 /**
- * main - prints alphabet in lowercase
+ * main - print if the number is postive, zero, or negative
  *
- *
- * Return: 0 if runs successful
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if ((c != 'e' && c != 'q'))
-putchar(c);
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
 }
 putchar('\n');
 return (0);
