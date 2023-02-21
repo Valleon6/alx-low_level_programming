cbf17a8d2a65d01e520eb48afcabb10662f1e5c5
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2023-02-21 14:33:26 -0800

    Task done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - function entry point
+ * Description - function that prints the n times table, starting with 0.
+ * @n: input
+ * Return - times table
+ * with extra space after each digit
+ */
+
+void print_times_table(int n)
+{
+	int row, column, num;
+	
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	else
+	{
+		for (row 0; row <= n; row++)
+		{
+			_putchar('0');
+
+			for (column = 1; column <= n; column++)
+			{
+				num = (row * column);
+				formatfunc(num);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+/**
+ * formatfunc - Entry point
+ * Description - function for formatting output
+ * @n: Input
+ * Return: void(Nothing)
+ */
+
+void formatfunc(int n)
+{
+	if (n <= 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
+
